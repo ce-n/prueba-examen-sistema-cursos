026c8bf919bdaa6d74da246e65105bf1560d8ef2
visita <visita@lab75.famaf.unc.edu.ar>
2019-11-13 16:39:17 -0300

    implementacion de la clase pregunta, metodo para verificar si la respuesta es correcta

--- src/curso/Pregunta.java
+++ src/curso/Pregunta.java
@@ -1,18 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package curso;
 
-/**
- *
- * @author Cecilia
- */
+package curso;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pregunta {
+    private String consigna;
     private float puntaje;
+    private ArrayList <String> opciones = new ArrayList <String> ();
+    private int opcionCorrecta;
+    
+    public Pregunta (String consigna, float puntaje ) {
+            this.consigna = consigna;
+            this.puntaje= puntaje;
+
+    }
     
     public float getPuntaje () {
         return puntaje;
     }
+    
+    public boolean esCorrecta (int respuesta) {
+
+        boolean respuestaCorrecta = false;
+    
+        if (respuesta == opcionCorrecta) {
+        
+        respuestaCorrecta = true;
+
+        }
+
+        return respuestaCorrecta;
+    
+    }
+
 }
