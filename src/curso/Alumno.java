2a59b73778300e4063edf9eff291484a1fc1440f
Cecilia <ce.narambuena@gmail.com>
2019-11-14 12:11:25 -0300

    documentación de la clase

--- src/curso/Alumno.java
+++ src/curso/Alumno.java
@@ -4,13 +4,20 @@ package curso;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase que representa a alumnos del curso
+ * 
+ * @author Cecilia
+ * @see curso.RendicionDeExamen
+ */
 public class Alumno {
     private List <RendicionDeExamen> examenesRendidos = new ArrayList <> ();
     private String nombre;
     private int dni;
     
     public Alumno (String nombre, int dni ) {
-        
+        this.nombre = nombre;
+        this.dni = dni;
     }
     
     public String getNombre (String nombre) {
@@ -22,6 +29,15 @@ public class Alumno {
         this.dni = dni;
         return dni;
     }
+    
+    /**
+     * Se agrega un nuevo examen en la lista examenes rendidos del alumno
+     * solo si el examen que se desea rendir está desaprobado
+     * 
+     * @param examen Examen necesario para generar una nueva rendicion
+     * @param respuestas Respuestas necesarias para generar una nueva rendicion
+     * @return Verdadero en caso de que se haya efectuado una nueva rendición de examen
+     */
     
     public boolean rendirExamen (Examen examen, ArrayList <Integer> respuestas) {
         
