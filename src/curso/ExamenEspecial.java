6d18d475eb1a4b2829ef420ca57cf691be8d4ad0
Cecilia <ce.narambuena@gmail.com>
2019-11-14 00:01:22 -0300

    Se implementó el metodo getTotalPenalizaciones para luego obtener el getPuntajeTotal

--- src/curso/ExamenEspecial.java
+++ src/curso/ExamenEspecial.java
@@ -1,12 +1,12 @@
 
 package curso;
 
-/**
- *
- * @author Cecilia
- */
+import java.util.ArrayList;
+
 public class ExamenEspecial extends Examen{
     
+    private ArrayList <Pregunta> preguntas = new ArrayList <> ();
+    private ArrayList <Integer> respuestas = new ArrayList <> ();
     private float penalizacionRespuestaIncorrecta;
     
     public ExamenEspecial (String tema, float puntajeAprobacion, float penalizacion) {
@@ -25,7 +25,34 @@ public class ExamenEspecial extends Examen{
     public float getPenalizacionRespuestaIncorrecta () {
         return penalizacionRespuestaIncorrecta;
     }
- 
-
     
+    @Override
+    public ArrayList <Integer> getRespuestas () {
+        this.respuestas = super.getRespuestas();
+        return respuestas;
+    }
+    
+    @Override
+    public ArrayList<Pregunta> getPreguntas() {
+        this.preguntas = super.getPreguntas();
+        return preguntas;
+    }
+
+    public float getTotalPenalizaciones (ArrayList <Integer> respuestas) {
+        float totalpenalizacion=0;
+        float acumuladorPenalizaciones=0;
+        for (int i=0; i<respuestas.size(); i ++) {
+            if (getPreguntas().get(i).esCorrecta(respuestas.get(i)) == false) {
+                acumuladorPenalizaciones++;
+                totalpenalizacion = getPenalizacionRespuestaIncorrecta()*acumuladorPenalizaciones;    
+            }    
+        }
+        return totalpenalizacion;
+    }
+    
+    public float getPuntajeTotal (ArrayList <Pregunta> preguntas, ArrayList <Integer> respuestas) {
+        float puntajeConPenalizacion;
+        puntajeConPenalizacion = super.getPuntajeTotal(preguntas) - getTotalPenalizaciones (respuestas);
+        return puntajeConPenalizacion;
+    }
 }
