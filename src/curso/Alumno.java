094c5151247aea2d1d4fedc68eb06f12f905cf34
visita <visita@lab75.famaf.unc.edu.ar>
2019-11-14 16:50:23 -0300

    correccion en el metodo rendirExamen

--- src/curso/Alumno.java
+++ src/curso/Alumno.java
@@ -42,18 +42,21 @@ public class Alumno {
     public boolean rendirExamen (Examen examen, ArrayList <Integer> respuestas) {
         
         boolean nuevoExamenAgregado = false;
+        boolean nuncaSeRindio = true;
         
-        for (int i=0; i<examenesRendidos.size(); i++) {
-            RendicionDeExamen examenRendido = examenesRendidos.get(i);    
-        
+        for (int i=0; i<examenesRendidos.size()-1; i--) {
+           
+            RendicionDeExamen examenRendido = examenesRendidos.get(i);
+            
             if (examenRendido.estaAprobado() == false) {
-                RendicionDeExamen nuevoExamenRendido = new RendicionDeExamen (examen, respuestas);
-                examenesRendidos.add(nuevoExamenRendido);
-                nuevoExamenAgregado = true;
+                  RendicionDeExamen nuevoExamenRendido = new RendicionDeExamen (examen, respuestas);
+                  examenesRendidos.add(nuevoExamenRendido);
+                  nuevoExamenAgregado = true;
+                    
             }
+
+            return nuevoExamenAgregado;   
         }
-        
-        return nuevoExamenAgregado;   
-    }
+           
     
-}
+    }
